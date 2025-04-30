6457ab4b87f1ef8cb5c9059e77da7552de1cb1ee
Андрей Смирный <smirnay2001@mail.ru>
2025-04-30 16:30:13 +0300

    Настроил роутинг с lazy загрузкой

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Counter from "./components/Counter";
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.Async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.Async";
 
 const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={"/"}>Главная страница</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };

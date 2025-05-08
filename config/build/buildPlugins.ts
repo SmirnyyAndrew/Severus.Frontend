6147e988591a7b6a53eb590433298fcb29b46b08
Андрей Смирный <smirnay2001@mail.ru>
2025-05-08 21:34:52 +0300

    Изменил порт bundleanalyzer на 8889

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -23,6 +23,6 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
+    new BundleAnalyzerPlugin({ openAnalyzer: false, analyzerPort: 8889 }),
   ];
 }

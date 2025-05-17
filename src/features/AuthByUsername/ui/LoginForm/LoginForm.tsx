b2e085a1687b46532c0eef47e1a1f7101b9e2761
Андрей Смирный <smirnay2001@mail.ru>
2025-05-17 20:00:13 +0300

    Удалил каретку из Input, добавил checkbox отображения пароля

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { HTMLInputTypeAttribute, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Button } from "shared/ui/Button";
@@ -11,6 +12,14 @@ interface LoginFormProps {
 
 export const LoginForm = ({ className }: LoginFormProps) => {
   const { t } = useTranslation();
+  const [isShowPassword, setIsShowPassword] = useState(true);
+  const [passwordType, setPasswordType] =
+    useState<HTMLInputTypeAttribute>("text");
+
+  const onToggleShowPassword = () => {
+    setPasswordType(passwordType === "text" ? "password" : "text");
+    setIsShowPassword((prev) => !prev);
+  };
 
   return (
     <div className={classNames(cls.LoginForm, {}, [className])}>
@@ -22,9 +31,18 @@ export const LoginForm = ({ className }: LoginFormProps) => {
       />
       <Input
         placeholder={t("enter_password")}
-        type="text"
+        type={passwordType}
         className={cls.input}
       />
+      <label className={cls.showPassword}>
+        {t("show_password")}
+        <input
+          type="checkbox"
+          id="is_show_password"
+          checked={isShowPassword}
+          onChange={onToggleShowPassword}
+        />
+      </label>
       <Button buttonTheme={ButtonTheme.OUTLINE} className={cls.loginBtn}>
         {t("login")}
       </Button>
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,11 +1,4 @@
-import {
-  ChangeEvent,
-  InputHTMLAttributes,
-  memo,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { InputHTMLAttributes, memo } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Input.module.scss";
 
@@ -35,54 +28,19 @@ export const Input = memo((props: InputProps) => {
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange?.(e.target.value);
   };
-  const ref = useRef<HTMLInputElement>();
-  const [isFocused, setIsFocused] = useState(false);
-  const [caretPosition, setCaretPosition] = useState(0);
-
-  useEffect(() => {
-    if (autofocus) {
-      setIsFocused(true);
-      ref.current?.focus();
-    }
-  }, [autofocus]);
-
-  const onBlur = () => {
-    setIsFocused(false);
-  };
-
-  const onFocus = () => {
-    setIsFocused(true);
-  };
-
-  const onSelectHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setCaretPosition(e.target.selectionStart || 0);
-  };
-
   return (
     <div className={classNames(cls.InputWrapper, {}, [className])}>
       {placeholder && (
         <div className={cls.placeholder}>{`${placeholder}>`}</div>
       )}
-      <div className={cls.caretWrapper}>
-        <input
-          {...otherProps}
-          value={value}
-          type={type}
-          ref={ref}
-          autoFocus={autofocus}
-          onSelect={onSelectHandler}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          onChange={onChangeHandler}
-          className={cls.input}
-        />
-        {isFocused && (
-          <span
-            style={{ left: `${caretPosition * 11}px` }}
-            className={cls.caret}
-          />
-        )}
-      </div>
+      <input
+        {...otherProps}
+        value={value}
+        type={type}
+        autoFocus={autofocus}
+        onChange={onChangeHandler}
+        className={cls.input}
+      />
     </div>
   );
 });

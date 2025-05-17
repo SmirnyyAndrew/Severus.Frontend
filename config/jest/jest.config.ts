0c9d508b43e216182c1cd5e9b74296994ccee439
Андрей Смирный <smirnay2001@mail.ru>
2025-05-18 00:21:42 +0300

    Установил игнорирование axios при jest тестировании

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -42,6 +42,9 @@ export default {
     __IS_DEV__: true,
   },
 
+  // Игнорирование axios при jest тестировании
+  transformIgnorePatterns: ["node_modules/(?!axios)/"],
+
   //Было расскоментировано
   // The glob patterns Jest uses to detect test files
   // testMatch: "<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)",
@@ -183,12 +186,6 @@ export default {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,

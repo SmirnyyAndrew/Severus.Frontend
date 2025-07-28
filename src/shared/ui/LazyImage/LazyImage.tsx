36b9d2179b721771c9e4d85271b888bd9d5ba74f
Андрей Смирный <smirnay2001@mail.ru>
2025-07-28 13:09:43 +0300

    LazyImage

---
+++ src/shared/ui/LazyImage/LazyImage.tsx
@@ -0,0 +1,49 @@
+import {
+  ImgHTMLAttributes,
+  memo,
+  ReactElement,
+  useLayoutEffect,
+  useState,
+} from "react";
+import { GENERAL_IMAGE_ERROR } from "shared/const/plugFiles";
+
+interface LazyImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+  className?: string;
+  fallback?: ReactElement;
+  errorFallback?: ReactElement;
+}
+
+export const LazyImage = memo((props: LazyImageProps) => {
+  const {
+    className,
+    src,
+    fallback,
+    errorFallback,
+    alt = "image",
+    ...otherProps
+  } = props;
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(true);
+
+  useLayoutEffect(() => {
+    const img = new Image();
+    img.src = src ?? GENERAL_IMAGE_ERROR;
+    img.onload = () => {
+      setIsLoading(false);
+    };
+    img.onerror = () => {
+      setIsLoading(false);
+      setHasError(true);
+    };
+  }, []);
+
+  if (isLoading && fallback) {
+    return fallback;
+  }
+
+  if (hasError && errorFallback) {
+    return errorFallback;
+  }
+
+  return <img className={className} alt={alt} src={src} {...otherProps} />;
+});

1c57b95162fd6103535fcc6d882a30e2e826bcbe
Андрей Смирный <smirnay2001@mail.ru>
2025-08-03 18:59:15 +0300

    fix ci cd

--- scripts/generate-visual-json-report.js
+++ scripts/generate-visual-json-report.js
@@ -1,9 +1,11 @@
 const { promisify } = require("util");
-const { readdir, writeFile } = require("fs");
+const { readdir, writeFile, access } = require("fs");
 const { join: joinPath, relative } = require("path");
+const { constants } = require("fs");
 
 const asyncReaddir = promisify(readdir);
 const writeFileAsync = promisify(writeFile);
+const accessAsync = promisify(access);
 
 const lokiDir = joinPath(__dirname, "..", ".loki");
 const actualDir = joinPath(lokiDir, "current");
@@ -11,21 +13,32 @@ const expectedDir = joinPath(lokiDir, "reference");
 const diffDir = joinPath(lokiDir, "difference");
 
 (async function main() {
-  const diffs = await asyncReaddir(diffDir);
+  let diffs = [];
+
+  try {
+    // Проверка, существует ли папка
+    await accessAsync(diffDir, constants.F_OK);
+    diffs = await asyncReaddir(diffDir);
+  } catch (err) {
+    // Папки нет — diffs остаются пустыми
+    console.warn(`No diffs found or folder "${diffDir}" is missing.`);
+  }
+
+  const report = {
+    newItems: [],
+    deletedItems: [],
+    passedItems: [],
+    failedItems: diffs,
+    expectedItems: diffs,
+    actualItems: diffs,
+    diffItems: diffs,
+    actualDir: relative(lokiDir, actualDir),
+    expectedDir: relative(lokiDir, expectedDir),
+    diffDir: relative(lokiDir, diffDir),
+  };
 
   await writeFileAsync(
     joinPath(lokiDir, "report.json"),
-    JSON.stringify({
-      newItems: [],
-      deletedItems: [],
-      passedItems: [],
-      failedItems: diffs,
-      expectedItems: diffs,
-      actualItems: diffs,
-      diffItems: diffs,
-      actualDir: relative(lokiDir, actualDir),
-      expectedDir: relative(lokiDir, expectedDir),
-      diffDir: relative(lokiDir, diffDir),
-    })
+    JSON.stringify(report, null, 2)
   );
 })();
